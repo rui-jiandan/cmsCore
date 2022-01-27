76fd3aba74b9a3d5089967fc4cf5d695c7990426
luorui <919180877@qq.com>
2022-01-27 20:52:26 +0800

    新增Excel数据源相关实体

---
+++ src/LinCms.Core/Entities/ExcelData/ExcelDataConfig.cs
@@ -0,0 +1,33 @@
+﻿using FreeSql.DataAnnotations;
+
+namespace LinCms.Entities
+{
+    /// <summary>
+    /// excel数据源配置
+    /// </summary>
+    [Table(Name ="excel_config")]
+    [Index("fileid_cellname", "fileid,cellname",true)]
+    [Index("fileid_dbcolumname", "fileid,dbcolumname", true)]
+    public class ExcelDataConfig: FullAduitEntity
+    {
+        /// <summary>
+        /// 文件Id
+        /// </summary>
+        public long FileId { get; set; }
+        /// <summary>
+        /// 列名
+        /// </summary>
+        [Column(StringLength = 100)]
+        public string CellName { get; set; }
+        /// <summary>
+        /// 列类型
+        /// </summary>
+        [Column(StringLength =30)]
+        public string CellType { get; set; }
+        /// <summary>
+        /// 数据库名称
+        /// </summary>
+        [Column(StringLength = 100)]
+        public string DbColumnName { get; set; }
+    }
+}
---
+++ src/LinCms.Core/Entities/ExcelData/ExcelFile.cs
@@ -0,0 +1,36 @@
+﻿using FreeSql.DataAnnotations;
+
+namespace LinCms.Entities
+{
+    /// <summary>
+    ///  excel数据源文件
+    /// </summary>
+    [Table(Name ="excel_file")]
+    public class ExcelFile : FullAduitEntity
+    {
+        /// <summary>
+        /// 文件名
+        /// </summary>
+        public string FileName { get; set; }
+
+        /// <summary>
+        /// 扩展名
+        /// </summary>
+        public string FileExtension { get; set; }
+
+        /// <summary>
+        /// 是否创建
+        /// </summary>
+        public bool IsCreate { get; set; }
+
+        /// <summary>
+        /// 文件路径
+        /// </summary>
+        public string FilePath { get; set; }
+
+        /// <summary>
+        /// 大小 KB
+        /// </summary>
+        public decimal? Size { get; set; }
+    }
+}
